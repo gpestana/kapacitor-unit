816042417cfd6ddedbd98b8fbaf94a548336670f
gpestana <g6pestana@gmail.com>
2016-10-25 00:18:31 +0300

    (fs) Parses test definitions and tick scripts

--- components/test_manager/lib/fs.js
+++ components/test_manager/lib/fs.js
@@ -16,13 +16,14 @@ const TESTS_DEF_FILE = process.env.TESTS_DEF_FILE;
 
 
 const load_test_data = () => {
-
 	return load_tests_def()
-		.map(tests_def => {_.map(load_scripts, _.keys(tests_def)); return tests_def;})
-		//.chain(prepare_scripts)
-		.map(data => {console.log('done'); return data});
-
-
+		.map(tests_def => {
+      _.keys(tests_def).map(key => {
+        const script = load_script(key).getOrElse({});
+        tests_def[key].script = script;
+      });
+      return tests_def;
+    });
 }
 
 // load_tests_def: -> Either(Error, Object)
@@ -35,19 +36,13 @@ const load_tests_def = () => {
   }
 };
 
-// load_scripts: Object -> Either(Error, Object);
-const load_scripts = (script_name) => {
-	console.log(script_name)
-	script_name = 'this'
-
-/*
+// load_script: String -> Either(Error, Object);
+const load_script = (script_name) => {
   try {
-    return Right(_.filter(f => path_lib.extname(f) == '.tick', 
-    	fs.readdirSync(SCRIPTS_DIRECTORY, 'utf8')));
+    return Right(fs.readFileSync(`${SCRIPTS_DIRECTORY}/${script_name}.tick`, 'utf8'));
   } catch (e) {
     return Left(e);
   }
-*/
 };
 
 
